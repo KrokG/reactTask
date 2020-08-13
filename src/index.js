046599182d2126884d056f10c9df7ebe9fe31c53
Gennady.Sharanov <ge.u1@yandex.ru>
2020-08-13 17:06:41 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+let page = 1; 
+
+function Page(props){
+	switch(props.page){
+		case 0: return creatMain();
+		case 1: return creatSlider();
+		default: return <h1>400 not found</h1>
+	}
+}
+
+//
+
+function creatMain(){
+	return (
+		<div>
+			<h1>Welcom to page</h1>
+			<button>slider</button>
+		</div>
+	)
+}
+
+//
+
+function creatSlider(){
+	return (
+		<div>
+			<h1>creatSlider</h1>
+			<button>main</button>
+		</div>
+	)
+}
+
+//
+
+function app(){
+	ReactDOM.render((
+		<BrowserRouter>
+			<Page page={page}/>
+			<Switch>
+				<Route exact path="/home" component={home} />
+			</Switch>
+		</BrowserRouter>
+	), document.getElementById('root'))
+}
+
+//
+
+app();
+
+let btn = document.getElementsByTagName('button')[0];
+console.log(btn);
+btn.onclick = function(event){
+	console.log('click');
+	if(page === 1){
+		page = 0;
+		app();
+	} else if(page === 0){
+		page = 1;
+		app();
+	}
+}
